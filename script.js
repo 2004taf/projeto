085652e43b63bf2fa3db0c4a36db6a1c4482b33d
Bagacinho-chan <95945441+bagacinho-chan@users.noreply.github.com>
2022-12-03 14:17:19 +0000

    changes on code

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
 $(document).ready(function(){
+    var state = true;
     $.fn.darkmode = function(){
         $("#img").click(function(){
-            var state = true;
-
             if (state){
                 $("body").animate({
                     "background-color": "black",
                     "color": "white",
                 }, 1000);
+                img = "images/dark mode.png";
+                $("#img").attr("src", img)
+                $("body").addClass("test");
+                $(".mensagem").addClass("back").removeClass("mensagem");
             } else {
                 $("body").animate({
                     "background-color": "white",
                     "color": "black",
                 }, 1000);
-            }
-            /*
-            var img;
-            if($(this).attr("src") == "images/white mode.png"){
-                img = "images/dark mode.png";
-                $(this).attr("src", img)
-                $("body").addClass("test");
-                $(".mensagem").addClass("back").removeClass("mensagem");
-            } else {
                 img = "images/white mode.png";
-                $(this).attr("src", img)
+                $("#img").attr("src", img)
                 $("body").removeClass("test");
                 $(".back").removeClass("back").addClass("mensagem");
             }
-            */
+            state = !state;
         });
     }
-    
     $("input[type=img]").darkmode();
     // AJAX PART
     var uName = $("input[name=user]").val();
@@ -42,14 +35,14 @@ $(document).ready(function(){
             url: 'process.php',
             data: {"user": uName, "pass": passwrd, "date": dat},
             success : function(data) {
-                if(("#mensagem").className === "mensagem"){
-                    $('.mensagem').html(data);
-                    $('.mensagem').show();
+                if($("#mensagem").hasClass('mensagem')){
+                    $(".mensagem").html(data);
+                    $(".mensagem").show();
                 } else {
-                    $('.back').html(data);
-                    $('.back').show();
+                    $(".back").html(data);
+                    $(".back").show();
                 }
-                
+                    
            },
             error: function (xhr,ajaxOptions,throwError){
                 alert("erro");
@@ -67,6 +60,10 @@ $(document).ready(function(){
     });
 
     $('#btn').click(function(){
-        $('.mensagem').toggle('clip');
+        if($("#mensagem").hasClass('mensagem')){
+            $('.mensagem').toggle('clip');
+        } else {
+            $('.back').toggle('clip');
+        }
     })
 });
