3d531d25a764b66bb0c50a055045407894e6919f
Bagacinho-chan <95945441+bagacinho-chan@users.noreply.github.com>
2022-12-11 22:59:47 +0000

    ok

--- script_info.js
+++ script_info.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
    $('#dados').load('process.php');
    $('#dados').show();
+
+   $('#highlight').click(function(){
+      $(".port").css("color", "red");
+   })
+
+   $('#amarilo').click(function(){
+      $('p').first().css("color", "green");
+      $(this).css("color", "yellow");
+   })
 })
