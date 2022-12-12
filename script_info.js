96b52cc18a67b9e1e16ea38d152d2e71d2b60b17
Bagacinho-chan <95945441+bagacinho-chan@users.noreply.github.com>
2022-12-12 12:25:35 +0000

    epah ta bom

--- script_info.js
+++ script_info.js
@@ -11,20 +11,21 @@ $(document).ready(function(){
       $(this).css("color", "yellow");
    })
 
-   function highlight(){
-      $(".port").find(".highlight").removeClass("highlight");
+   $.fn.highlight = function(){
+      $('#highlightButton').click(function(){
+         $(".port").find(".highlight").removeClass("highlight");
 
-    var searchword = $("txtBox").val();
+         var searchword = $("#text").val();
 
-    var custfilter = new RegExp(searchword, "ig");
-    var repstr = "<span class='highlight'>" + searchword + "</span>";
+         var custfilter = new RegExp(searchword, "ig");
+         var repstr = "<span class='highlight'>" + searchword + "</span>";
 
-    if (searchword != "") {
-        $('body').each(function() {
-            $(this).html($(this).html().replace(custfilter, repstr));
-        })
-    }
+         if (searchword != "") {
+            $('body').each(function() {
+               $(this).html($(this).html().replace(custfilter, repstr));
+            });
+         }
+      });
    }
-
-   
+   $('#highlightButton').highlight();
 })
